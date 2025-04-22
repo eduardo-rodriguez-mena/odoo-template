cf664ab8d2ac2f09ccd25ab80459b8485b1ab1a5
eduardo <eduardo.rod@gmail.com>
2025-04-21 22:20:20 -0600

    Actualizar scritp renovar cert

--- odoo-db/cron.weekly/db_certbot_renew.sh
+++ odoo-db/cron.weekly/db_certbot_renew.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /app/odoo-db
 docker container start odoo-db-certbot-1 && sleep 45
-docker container start odoo-db-perm-1 
-docker container stop odoo-db-db-1 
-docker container start odoo-db-db-1 
+docker compose stop
+docker compose up -d
